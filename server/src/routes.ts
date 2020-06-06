30c89f804f850d815bd93d177735d59dd9bdfdcd
Igor Martins <figortmartins@gmail.com>
2020-06-06 09:33:46 -0300

    Now the api provide the upload functionalite.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,15 +1,20 @@
 import express from 'express'
 
+import multer from 'multer'
+import multerConfig from './config/multer'
+
 import PointsController from './controllers/PointsController'
 import ItemsController from './controllers/ItemsController'
 
 const routes = express.Router()
+const upload = multer(multerConfig)
+
 const pointsControllers = new PointsController()
 const itemsController = new ItemsController()
 
 routes.get('/items', itemsController.index)
 
-routes.post('/points', pointsControllers.create)
+routes.post('/points', upload.single('image'), pointsControllers.create)
 routes.get('/points/:id', pointsControllers.show)
 routes.get('/points', pointsControllers.index)
 
