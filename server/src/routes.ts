e3d0c25a45ec19c1a81e75ebb9e66a0bcc07a07f
Igor Martins <figortmartins@gmail.com>
2020-06-07 10:08:45 -0300

    Added validation of post requests in our route.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { celebrate, Joi } from 'celebrate'
 
 import multer from 'multer'
 import multerConfig from './config/multer'
@@ -14,7 +15,23 @@ const itemsController = new ItemsController()
 
 routes.get('/items', itemsController.index)
 
-routes.post('/points', upload.single('image'), pointsControllers.create)
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.string().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required()
+    })
+  }),
+  pointsControllers.create
+)
 routes.get('/points/:id', pointsControllers.show)
 routes.get('/points', pointsControllers.index)
 
