19bf5cf237cad912463309431da32825f9adeffa
Igor Martins <figortmartins@gmail.com>
2020-06-08 01:19:39 -0300

    Updates

---
+++ precache-manifest.754ed146a9cae03b633f6ab6828db31a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd697e02fc0cbd7c48100396369e526e",
+    "url": "/ecoleta/index.html"
+  },
+  {
+    "revision": "b8bbccefa5a90b4ec37f",
+    "url": "/ecoleta/static/css/main.cbc78667.chunk.css"
+  },
+  {
+    "revision": "5489b116af0c5368b557",
+    "url": "/ecoleta/static/js/2.b2690283.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/ecoleta/static/js/2.b2690283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8bbccefa5a90b4ec37f",
+    "url": "/ecoleta/static/js/main.3222e716.chunk.js"
+  },
+  {
+    "revision": "7e4de1f158a5cf6f86a8",
+    "url": "/ecoleta/static/js/runtime-main.8591051e.js"
+  },
+  {
+    "revision": "fe2283e1f17af7f6d2889b5692937968",
+    "url": "/ecoleta/static/media/home-background.fe2283e1.svg"
+  },
+  {
+    "revision": "47c4581a1dc536c1ba57fddafd6671b9",
+    "url": "/ecoleta/static/media/logo.47c4581a.svg"
+  }
+]);
