ab54502a74c70467fea48a205f238cff62fe7d7a
Igor Martins <figortmartins@gmail.com>
2020-06-08 20:05:55 -0300

    Now is resposive for all screens.

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Route, BrowserRouter } from 'react-router-dom'
+import { Route, HashRouter } from 'react-router-dom'
 
 import Home from './pages/Home'
 import CreatePoint from './pages/CreatePoint'
 
 const Routes = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Route component={Home} path="/" exact />
       <Route component={CreatePoint} path="/create-point" />
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
